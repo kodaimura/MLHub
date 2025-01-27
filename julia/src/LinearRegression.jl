7f33bb955fa5139961da6da3e53a027cfc18704b
kodaimura <kodaimura123@gmail.com>
2025-01-27 16:46:09 +0900

    minor fix

--- julia/src/LinearRegression.jl
+++ julia/src/LinearRegression.jl
@@ -31,7 +31,7 @@ function loss_elastic_net(model, features, labels; lambda1=0.01, lambda2=0.01)
     return mse_loss + l1_penalty + l2_penalty
 end
 
-function train_model!(f_loss, model, features, labels, learning_rate=0.01)
+function train_model!(f_loss, model, features, labels; learning_rate=0.01)
     dLdm, _, _ = gradient(f_loss, model, features, labels)
     @. model.weight = model.weight - learning_rate * dLdm.weight
     @. model.bias = model.bias - learning_rate * dLdm.bias
@@ -57,22 +57,42 @@ function train_until_converged!(f_loss, model, features, labels; max_epochs=1000
 end
 
 function main()
-    #サンプルデータ
-    x = rand(Float32, 5, 100)
-    f(x) = @. 3 * x[1, :] + 2 * x[2, :] - x[3, :] + 4 * x[4, :] - 2 * x[5, :] + 1
-    y = reshape(f(x), 1, 100)
+    #単回帰サンプル
+    (function()
+        x = hcat(collect(Float32, -3:0.1:3)...)
+        f(x) = @. 3 * x
+        y = reshape(f(x), 1, 61)
+        x = x .+ reshape(rand(Float32, 61), (1, 61))
+        y = y .+ reshape(rand(Float32, 61), (1, 61))
 
-    f_loss = (model, features, labels) -> loss_ridge(model, features, labels; lambda=0.05)
-    model = Flux.Dense(5 => 1)
-    features = x
-    labels = y
-    train_until_converged!(f_loss, model, features, labels)
-    predicted_values = model(features)
+        f_loss = (model, x, y) -> loss_ridge(model, x, y; lambda=0.05)
+        model = Flux.Dense(1 => 1)
+        train_until_converged!(f_loss, model, x, y)
+        predicted_values = model(x)
 
-    plot(1:100, labels[1, :], label="True values", lw=2, title="Model Training")
-    plot!(1:100, predicted_values[1, :], label="After Training", lw=2)
+        plot(vec(x), vec(y), seriestype = :scatter, label="True values", title="Model Training")
+        plot!((x) -> model.bias[1] + model.weight[1] * x, label="After Training", lw=2)
 
-    savefig("LinearRegression.png")
+        savefig("LinearRegression.png")
+    end)()
+
+    #重回帰サンプル
+    (function()
+        x = rand(Float32, 5, 100)
+        f(x) = @. 3 * x[1, :] + 2 * x[2, :] - x[3, :] + 4 * x[4, :] - 2 * x[5, :] + 1
+        y = reshape(f(x), 1, 100)
+        y = y .+ reshape(rand(Float32, 100), (1, 100))
+
+        f_loss = (model, x, y) -> loss(model, x, y)
+        model = Flux.Dense(5 => 1)
+        train_until_converged!(f_loss, model, x, y; tolerance=1e-6)
+        predicted_values = model(x)
+
+        plot(1:100, y[1, :], seriestype = :scatter, label="True values", title="Model Training")
+        plot!(1:100, predicted_values[1, :], label="After Training")
+
+        savefig("LinearRegression2.png")
+    end)()
 end
 
 main()
